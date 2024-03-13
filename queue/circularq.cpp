cae05e4814fe5fe6dfdeacc3889fc8627ce787e6
Aarchi Jain <aarchi.jain0409@gmail.com>
2024-03-14 00:47:34 +0530

    circular queue

--- queue/circularq.cpp
+++ queue/circularq.cpp
@@ -1,62 +1,136 @@
-/*
-class CircularQueue{
-    int *arr;
-    int front;
-    int rear;
-    int size;
-    
-    public:
-    // Initialize your data structure.
-    CircularQueue(int n){
-        size = n;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class CircularQueue
+{
+public:
+    int *arr;  // Pointer storage
+    int size;  // Array size
+    int front; // Index of front element
+    int rear;  // Index of rear element
+
+    CircularQueue(int size)
+    {
+        this->size = size;
         arr = new int[size];
-        front = rear = -1;
+        front = -1;
+        rear = -1;
     }
 
-    // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
-    bool enqueue(int value){
-        //to check whther queue is full
-        if( (front == 0 && rear == size-1) || (rear == (front-1)%(size-1) ) ) {
-            //cout << "Queue is Full";
-            return false;
-        }
-        else if(front == -1) //first element to push
+    ~CircularQueue()
+    {
+        delete[] arr;
+    }
+
+    void push(int data)
+    {
+        if ((front == 0 && rear == size - 1) || (rear == (front - 1) % size))
         {
-			front = rear = 0;
-            
+            cout << "Q is full,can't insert" << endl;
         }
-        else if(rear == size-1 && front != 0) {
-            rear = 0; //to maintain cyclic nature
+        else if (front == -1)
+        {
+            front = rear = 0;
+            arr[rear] = data;
+        }
+        else if (rear == size - 1 && front != 0)
+        {
+            rear = 0;
+            arr[rear] = data;
         }
         else
-        {//normal flow
+        {
             rear++;
+            arr[rear] = data;
         }
-        //push inside the queue
-        arr[rear] = value;
-        
-        return true;
     }
 
-    // Dequeues top element from queue. Returns -1 if the stack is empty, otherwise returns the popped element.
-    int dequeue(){
-        if(front == -1){//to check queue is empty
-            //cout << "Queue is Empty " << endl;
-            return -1;
+    void pop()
+    {
+        if (front == -1)
+        {
+            // Queue is empty
+            cout << "Q is empty" << endl;
         }
-        int ans = arr[front];
-        arr[front] = -1;
-        if(front == rear) { //single element is present
+        else if (front == rear)
+        {
+            arr[front] = -1;
             front = rear = -1;
         }
-        else if(front == size - 1) {
-            front = 0; //to maintain cyclic nature
+        else if (front == size - 1)
+        {
+            front = 0;
         }
         else
-        {//normal flow
+        {
             front++;
         }
-        return ans;
+    }
+
+    int getFront()
+    {
+        if (front == -1)
+        {
+            cout << "Q is empty" << endl;
+            return -1;
+        }
+        else
+        {
+            return arr[front];
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (front == rear)
+        {
+            // Queue is empty
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    int getSize()
+    {
+        if (front == -1)
+            return 0;
+        if (rear >= front)
+            return rear - front + 1;
+        else
+            return size - front + rear + 1;
     }
 };
-*/
+
+int main()
+{
+    // * Circular Queue
+    // ! rear circular m element pr hogi
+
+    CircularQueue q(10);
+
+    q.push(5);
+    q.push(15);
+    q.push(25);
+
+    cout << "Size of queue is: " << q.getSize() << endl;
+
+    q.pop();
+    cout << "Size of queue after pop: " << q.getSize() << endl;
+
+    cout << "Front element is: " << q.getFront() << endl;
+
+    if (q.isEmpty())
+    {
+        cout << "Q is empty" << endl;
+    }
+    else
+    {
+        cout << "Q is non-empty" << endl;
+    }
+
+    return 0;
+}
